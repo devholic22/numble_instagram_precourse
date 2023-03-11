148666cbf98d6029cc05999b4c0727dd4e27e5be
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-03-11 16:50:52 +0900

    initial setting

---
+++ src/main/java/numble/instagramprecourse/InstagramPrecourseApplication.java
@@ -0,0 +1,13 @@
+package numble.instagramprecourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramPrecourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramPrecourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/numble/instagramprecourse/InstagramPrecourseApplicationTests.java
@@ -0,0 +1,13 @@
+package numble.instagramprecourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramPrecourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
