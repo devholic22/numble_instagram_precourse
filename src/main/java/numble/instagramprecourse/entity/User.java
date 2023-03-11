61deaaaf226eb40c484d0afcfd03748c7066e6c7
Hyunjoon Choi <90085154+devholic22@users.noreply.github.com>
2023-03-11 23:47:57 +0900

    User 엔티티 기본 설정

---
+++ src/main/java/numble/instagramprecourse/entity/User.java
@@ -0,0 +1,31 @@
+package numble.instagramprecourse.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
